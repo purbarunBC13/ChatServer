6324cca7f0dcfd13c372da0023f02943a284b125
purbarunBC13 <purbarunmondal@gmail.com>
2024-11-10 15:55:19 +0530

    testing cookies

--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -1,17 +1,18 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-    // console.log(req.cookies);
-    const token = req.cookies.jwt;
-    if (!token) {
-        return res.status(401).send("You are unauthorized");
+  console.log(req.cookies);
+  const token = req.cookies.jwt;
+  console.log(req.cookies.jwt);
+  if (!token) {
+    return res.status(401).send("You are unauthorized");
+  }
+  jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
+    if (err) {
+      return res.status(401).send("Token is not valid");
     }
-    jwt.verify(token,process.env.JWT_KEY,async(err,payload)=>{
-        if(err){
-            return res.status(401).send("Token is not valid");
-        }
-        // console.log(payload);
-        req.userId = payload.userId;
-        next();
-    })
+    // console.log(payload);
+    req.userId = payload.userId;
+    next();
+  });
 };
